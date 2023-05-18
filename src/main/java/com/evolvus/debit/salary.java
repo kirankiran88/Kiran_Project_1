25d56acd47ea0a457773de9f00d70fb481cd00ce
kirann-paycorp <kiran.n@paycorp.io>
2023-05-18 17:28:27 +0530

    salary class created

---
+++ src/main/java/com/evolvus/debit/salary.java
@@ -0,0 +1,13 @@
+package com.evolvus.debit;
+
+public class salary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("enter your salary ");
+    if( true) {
+    	System.out.println("");
+    }
+	}
+
+}
