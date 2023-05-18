b61bd605418e922816556ade230d59f667d102d8
kirann-paycorp <kiran.n@paycorp.io>
2023-05-18 12:58:03 +0530

    Updated code with new changes

--- src/main/java/com/evolvus/debit/beditamt.java
+++ src/main/java/com/evolvus/debit/beditamt.java
@@ -5,6 +5,8 @@ public class beditamt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("your debited amount is _____");
+      
+      System.out.println("csggsdjh lsvyud hjdvs jhbg yggs");
 	}
 
 }
