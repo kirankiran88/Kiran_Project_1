4e397ad3ee3fbae77c2983780d3bb0948d13f9ae
kirann-paycorp <kiran.n@paycorp.io>
2023-05-18 12:25:15 +0530

    debited cmtd

---
+++ src/main/java/com/evolvus/debit/beditamt.java
@@ -0,0 +1,10 @@
+package com.evolvus.debit;
+
+public class beditamt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("your debited amount is _____");
+	}
+
+}
